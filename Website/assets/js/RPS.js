453277bbfc769b311ce4dc70c6249b21c5a74cd0
Teddy Todorov <forteddyt@gmail.com>
2018-03-06 03:08:09 -0500

    working on js + very basic front-end

--- Website/assets/js/RPS.js
+++ Website/assets/js/RPS.js
@@ -1,19 +1,155 @@
-window.addEventListener('load', function() {
+// Modified from https://github.com/MetaMask/faq/blob/master/DEVELOPERS.md#partly_sunny-web3---ethereum-browser-environment-check
+var eth 
+var moveMap = new Map();
+    moveMap.set('rock', 0);
+    moveMap.set('paper', 1);
+    moveMap.set('scissor', 2);
 
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    // Use Mist/MetaMask's provider
-    web3js = new Web3(web3.currentProvider);
+window.addEventListener('load', function() {
+  eth = new Eth(new Eth.HttpProvider('http://localhost:8501'));
+
+  if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
+    eth.setProvider(window.web3.currentProvider);
   } else {
-    console.log('No web3? You should consider trying MetaMask!')
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8501"));
+    eth.setProvider(provider);
   }
 
+  if(eth !== 'undefined'){
   // Now you can start your app & access web3 freely:
-  startApp()
+    startApp()
+  }else{
+    notConnected()
+  }
 })
 
+// If web3 injection cannot be confirmed
+function notConnected(){
+  console.log("Not connected")
+}
+
+// Starts app after web3 injection has been confirmed
 function startApp(){
-  console.log("App started");
+  checkNetwork();
+  populateGames();
+  console.log("App started")
+}
+
+// Checks the MetaMask network to ensure user is on the mainnet 
+function checkNetwork(){
+  web3.version.getNetwork((err, netId) => {
+    switch (netId) {
+      case "1":
+        // In mainnet
+        console.log('This is mainnet')
+        break
+      case "3":
+        // In Ropset test network
+        console.log('This is the ropsten test network.')
+        break
+      default:
+        // In some other network (may or may not be unknown)
+        console.log('This is some other network.')
+    }
+  })
+}
+
+// List the ongoing games that are on the block chain
+function populateGames(){
+
+}
+
+// Fired when rock/paper/scissor is played (ie, 'Rock, Paper, Scissor... Shoot!')
+function shoot(btn){
+  eth.accounts(function(accountsError, accounts)
+  {
+    // Obtain player's account from ethjs
+    if(accountsError){
+      return
+    }
+
+    console.log(accounts)
+
+    player = accounts[0]
+    move = btn.id
+    opponent = $(btn).parent().parent().attr('id')
+  })
+  .then(function()
+  {
+    // Make sure the person joining this game is NOT person who owns this game
+    if(player == opponent){
+      throw new Error("Cannot play against yourself!")
+    }
+
+    // Then contact backend which move has been made by which player
+    console.log(player + " played " + move + "(" + moveMap.get(move) + ") against " + opponent + "!")
+  })
+  .catch(function(err)
+  {
+    // If error occurs, catch it
+    console.error(err)
+  })
+}
+
+function createMatch(){
+  eth.accounts(function(accountsError, accounts)
+  {
+    // Obtain player's account from ethjs
+    if(accountsError){
+      return
+    }
+    acc = accounts[0]
+    ethAmount = $("#ethAmount").val()
+  })
+  .then(createBlock(acc, ethAmount)) // Create match on the blockchain
+  .then(createListing(acc, ethAmount)) // Create a listing of the match on the website
+  .catch(function(err)
+  {
+    console.error(err)
+  })
+}
+
+function createBlock(acc, ethAmount){
+  if(ethAmount !== 'undefined' && ethAmount > 0){
+  }else{
+    throw new Error("Invalid Etherium Amount")
+  }
+}
+
+// Creates the html object for a specific match
+function createListing(acc, ethAmount){
+    var div = document.createElement('div');
+    var txtDiv = document.createElement('div'); // Text div
+    var btnDiv = document.createElement('div'); // Button div
+    var spcDiv = document.createElement('div'); // Spacer div
+
+    txtDiv.id = 'txtDiv'
+    txtDiv.innerHTML =
+      `
+        <span>Challenger: </span>
+        <span id="chalAcc">${acc}</span>
+        <span>---</span>
+        <span>Amount: </span>
+        <span id="chalAmount">${ethAmount}</span>
+        <span> Eth</span>
+      `
+
+    btnDiv.id = 'btnDiv'
+    btnDiv.innerHTML =
+      `
+        <button id="rock" onclick="shoot(this)">Rock</button>
+        <button id="paper" onclick="shoot(this)">Paper</button>
+        <button id="scissor" onclick="shoot(this)">Scissor</button>
+      `
+
+    spcDiv.id = 'spcDiv'
+    spcDiv.innerHTML =
+      `
+      <br><br>
+      `
+
+    div.id = acc
+    div.appendChild(txtDiv);
+    div.appendChild(btnDiv);
+    div.appendChild(spcDiv);
+    $('#gameList').append(div);
 }
