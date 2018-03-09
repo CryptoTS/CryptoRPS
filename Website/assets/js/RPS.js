9f932280fc9f1238c1ec984f0409a094210a399c
Teddy Todorov <forteddyt@gmail.com>
2018-03-09 18:48:12 -0500

    Populates initial RPS games from contract

--- Website/assets/js/RPS.js
+++ Website/assets/js/RPS.js
@@ -7,8 +7,27 @@ var moveMap = new Map();
 const abi = [
   {
     "constant": true,
+    "inputs": [
+      {
+        "name": "_player",
+        "type": "address"
+      }
+    ],
+    "name": "getNumActiveMatchesFor",
+    "outputs": [
+      {
+        "name": "activeMatches",
+        "type": "uint32"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
     "inputs": [],
-    "name": "numActiveMatches",
+    "name": "activeMatchCounter",
     "outputs": [
       {
         "name": "",
@@ -122,12 +141,29 @@ const abi = [
   },
   {
     "constant": true,
-    "inputs": [],
-    "name": "getNumActiveMatches",
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "_matches",
     "outputs": [
       {
-        "name": "activeMatches",
-        "type": "uint32"
+        "name": "creator",
+        "type": "address"
+      },
+      {
+        "name": "opponent",
+        "type": "address"
+      },
+      {
+        "name": "wager",
+        "type": "uint256"
+      },
+      {
+        "name": "outcome",
+        "type": "int8"
       }
     ],
     "payable": false,
@@ -178,13 +214,8 @@ const abi = [
   },
   {
     "constant": true,
-    "inputs": [
-      {
-        "name": "_player",
-        "type": "address"
-      }
-    ],
-    "name": "getNumActiveMatches",
+    "inputs": [],
+    "name": "getNumActiveMatchesFor",
     "outputs": [
       {
         "name": "activeMatches",
@@ -288,9 +319,11 @@ const abi = [
     "name": "MatchKilled",
     "type": "event"
   }];
-const address = "0xFdFeE25A108d79453c3d116D5fb9681AdeD81DE1";
+const address = "0xd7113Be484Ff03DC6143de9FEa0a533d69cA99a2";
 var contract;
+var activeMatches = []  // Array to store all active matches
 var canCreateMatch = true; // The player can only have one match up at a time
+let curAcc // current account
 
 window.addEventListener('load', function() {
   web3js = new Web3(Web3.givenProvider || "http://localhost:8501");
@@ -300,20 +333,33 @@ window.addEventListener('load', function() {
 
     web3js.eth.getAccounts()
     .then(function(accounts){
-      var activeMatchesOps = {
-        from: accounts[0]
+      curAcc = accounts[0]
+      let activeMatchesOps = {
+        from: curAcc
       }
 
       // Set canCreateMatch based on contract data
-      contract.methods.getNumActiveMatches().call(activeMatchesOps)  // Call hasActiveMatch() w/ specified options
-      .then(function (result){
-        canCreateMatch = !(result > 0)  // If this player has more than 0 active matches, they cannot create matches 
-        console.log(result)
+      contract.methods.getNumActiveMatchesFor().call(activeMatchesOps)  // Call hasActiveMatch() w/ specified options
+      .then(function (numActiveMatches){
+        canCreateMatch = !(numActiveMatches > 0)  // If this player has more than 0 active matches, they cannot create matches
       })
     })
-
-    startApp() // Then after obtaining, startApp
-    // Now you can start your app & access web3 freely:
+    .then(function(){ // After initial account is obtained
+      startApp()
+      
+      window.setInterval(function() {
+        web3js.eth.getAccounts()
+        .then(function(accounts){
+          if(curAcc != accounts[0])
+          {
+            location.reload() // Set a Periodical check to see if the account has changed and refresh the page if it has
+          }
+        })
+      }, 100)
+    })
+    .catch(function(err){
+      console.error(err)
+    })
   }else{
     notConnected()
   }
@@ -327,48 +373,69 @@ function notConnected(){
 
 // Starts app after web3 injection has been confirmed
 function startApp(){
-  checkNetwork()
+  checkNetworkProm = new Promise(checkNetwork)
+  activeGamesSortedProm = new Promise(getActiveGamesSorted)
+  checkNetworkProm
+  .then(function(networkId){
+    console.log("networkId " + networkId)
+    activeGamesSortedProm
+    .then(function(activeMatchesSorted){
+      for(let i = 0; i < activeMatchesSorted.length; i++){
+        appendListing(activeMatchesSorted[i])
+      }
+    })
+  })
+  .catch(function(err){
+    console.log(err)
+  })
 }
 
 // Checks the MetaMask network to ensure user is on the mainnet 
-function checkNetwork(){
+function checkNetwork(resolve, reject){
   web3js.eth.net.getId((err, netId) => {
     switch (netId) {
       case 1:
         // In mainnet
         console.log('This is mainnet')
+        resolve(1)
         break
       case 3:
         // In Ropset test network
         console.log('This is the ropsten test network.')
+        resolve(3)
         break
       default:
         // In some other network (may or may not be unknown)
-        console.log('This is some other network.')
+        reject(Error("This is an unknown network"))
     }
   })
 }
 
 // List the ongoing games that are on the block chain
-function populateGames(){
-  contract.getMatchIDsOfAddress("0x9CD16ef861Ee022A214E54d1ef9321dDA0ecD222")
-  // {
-  //   var idList = []
-  //   for(var i = 0; i < ret.length; i++){
-  //     idList[i] = ret[i].toNumber()
-  //   }
-  //   console.log(idList)
-
-  //   for(var i = 0; i < idList.length; i++){
-  //     contract.getMatch(idList[i], function(err, ret){
-  //       console.log("Match info = " + ret)
-  //       console.log(ret[0]);  // creator
-  //       console.log(ret[1]);  // opponent
-  //       console.log(ret[2].toNumber());  // wager
-  //       console.log(ret[3].toNumber());  // outcome
-  //     })
-  //   }
-  // })
+function getActiveGamesSorted(resolve, reject){
+  let activeMatchProms = [] // Array to store all active match requests (ie promises)
+  
+  contract.methods.getActiveMatchIDs().call() // Get all active match IDs from contract
+  .then(function (activeMatchIDs){  // When the activeMatchIDs (which is an array) is given back to us...
+    // console.log("Active Match IDs: ")
+    // console.log(activeMatchIDs)
+    for(let i = 0; i < activeMatchIDs.length; i++){ // Go through each activeMatchID
+      activeMatchProms.push(contract.methods.getMatch(activeMatchIDs[i]).call()  // Get each match associated to that active match ID
+      .then(function(matchInfo){ // Push each call (which is a promise) onto active match promises array
+        activeMatches.push(matchInfo) // Upon each getMatch completion, push that matchIDs matchInfo onto the activeMatches array
+      }))
+    }
+  })
+  .then(function(){ // Once all getMatch requests (ie promises) have been pushed onto the activeMatchProms array
+    Promise.all(activeMatchProms) // Once all promises in the activeMatchProms are resolved
+    .then(()=>{ // Then...
+      // console.log("All are complete!!")
+      resolve(activeMatches.sort(_compareByEthDesc))  // Resolve this function with the sorted matches
+    })
+  })
+  .catch(function(err){
+    reject(err) // If any error occurs, reject this function with that error
+  })
 }
 
 // Fired when rock/paper/scissor is played (ie, 'Rock, Paper, Scissor... Shoot!')
@@ -404,22 +471,18 @@ function shoot(btn){
 }
 
 function createMatch(){
-  var acc
-  web3js.eth.getAccounts(function(accountsError, accounts)
-  {
-    // Obtain player's account from ethjs
-    if(accountsError){
-      throw new Error(accountsError)
-    }else if(!canCreateMatch){
-      throw new Error("Can only have one match at a time")
+  let acc
+  let checkCreation = new Promise(function (resolve, reject){
+    if(!canCreateMatch){
+      reject(Error("Can only have one match at a time"))
     }
-    acc = accounts[0]
-    ethAmount = $("#ethAmount").val() // In Eth, need to be converted to Wei
+    resolve($("#ethAmount").val()) // In Eth, need to be converted to Wei
   })
-  .then(function (){  // Create rps match on the blockchain
+
+  checkCreation.then(function (ethAmount){  // Create rps match on the blockchain
     if(ethAmount !== 'undefined' && ethAmount > 0){
-      var createMatchOps = {
-        from: acc,
+      let createMatchOps = {
+        from: curAcc,
         value: web3js.utils.toWei(ethAmount, 'ether')  // Solidity Contract is payable with Wei, not ether
       }
 
@@ -429,21 +492,13 @@ function createMatch(){
         console.log("Starting transaction...")
       })
       .on('receipt', function(receipt){  // When blockchain has finished function execution
+        console.log("receipt: ")
         console.log(receipt)
-        console.log(receipt.from)
-        console.log(receipt.returnValues)
       })
-      .on('error', function(rec){
+      .on('error', function(receipt){
         canCreateMatch = true // If transaction errors out, player can create a match again
         console.log("Errored out")
-        if(rec){
-          console.log(rec)
-          // ran out of gas
-        }else{
-          console.log("else")
-          console.log(rec)
-          // some other error
-        }
+        throw new Error(receipt)
       })
     }else{
       throw new Error("Invalid Etherium Amount")
@@ -455,42 +510,71 @@ function createMatch(){
   })
 }
 
+// Appends the html object for a specific match to the end of the game list
+function appendListing(match){
+  // Get listing data from match
+  let creator = match.creator
+  let opponent = (creator == match.opponent) ? "No Opponent" : match.opponent  // If creator is the opponent, then they're looking for an opponent
+  let wager = web3js.utils.fromWei(web3js.utils.toBN(match.wager), "ether")  // Convert Wei amount to Ether amount. Easier to understand for user
 
-// Creates the html object for a specific match
-function createListing(acc, ethAmount){
-    var div = document.createElement('div');
-    var txtDiv = document.createElement('div'); // Text div
-    var btnDiv = document.createElement('div'); // Button div
-    var spcDiv = document.createElement('div'); // Spacer div
+  // Create listing elements
+  let div = document.createElement('div');
+  let txtDiv = document.createElement('div'); // Text div
+  let btnDiv = document.createElement('div'); // Button div
+  let spcDiv = document.createElement('div'); // Spacer div
 
-    txtDiv.id = 'txtDiv'
-    txtDiv.innerHTML =
-      `
-        <span>Challenger: </span>
-        <span id="chalAcc">${acc}</span>
-        <span>---</span>
-        <span>Amount: </span>
-        <span id="chalAmount">${ethAmount}</span>
-        <span> Eth</span>
-      `
+  txtDiv.id = 'txtDiv'
+  txtDiv.innerHTML =
+    `
+      <span>Challenger: </span>
+      <span id="chalAcc">${creator}</span>
+      <span>---</span>
+      <span>Amount: </span>
+      <span id="chalAmount">${wager}</span>
+      <span> Eth</span>
+    `
 
-    btnDiv.id = 'btnDiv'
-    btnDiv.innerHTML =
-      `
-        <button id="rock" onclick="shoot(this)">Rock</button>
-        <button id="paper" onclick="shoot(this)">Paper</button>
-        <button id="scissor" onclick="shoot(this)">Scissor</button>
-      `
+  btnDiv.id = 'btnDiv'
+  btnDiv.innerHTML =
+    `
+      <button id="rock" onclick="shoot(this)">Rock</button>
+      <button id="paper" onclick="shoot(this)">Paper</button>
+      <button id="scissor" onclick="shoot(this)">Scissor</button>
+    `
 
-    spcDiv.id = 'spcDiv'
-    spcDiv.innerHTML =
-      `
-      <br><br>
-      `
+  spcDiv.id = 'spcDiv'
+  spcDiv.innerHTML =
+    `
+    <br><br>
+    `
 
-    div.id = acc
-    div.appendChild(txtDiv);
-    div.appendChild(btnDiv);
-    div.appendChild(spcDiv);
-    $('#gameList').append(div);
+  div.id = creator
+  div.appendChild(txtDiv);
+  div.appendChild(btnDiv);
+  div.appendChild(spcDiv);
+  $('#gameList').append(div);
 }
+
+// Inserts the html object for a specific match to the ordered location in the game list
+// Game list is ordered by etherium amount, descending
+function insertListing(acc, ethAmount){
+  let gameListChildren = document.getElementById('gameList').getElementByTagName('div') // Get all children divs of the gameList
+  for(let i = 0; i < gameListChildren.length; i++){
+    gameListChild = gameListChildren[i]
+    console.log(gameListChild.getElementById('txtDiv').getElementById('chalAmount'))
+  }
+}
+
+/** HELPER FUNCTIONS **/
+// Compares two matches by their wager amounts. Uses BN.js to safeguard 
+// Defaults ordering in descending order (highest to lowest)
+function _compareByEthDesc(matchA, matchB){
+  return web3js.utils.toBN(matchA.wager).cmp(web3js.utils.toBN(matchB.wager)) * -1;
+}
+
+// Compares two matches by their wager amounts. Uses BN.js to safeguard 
+// Defaults ordering in ascending order (lowest to highest)
+function _compareByEthAsc(matchA, matchB){
+  return web3js.utils.toBN(matchA.wager).cmp(web3js.utils.toBN(matchB.wager));
+}
+
